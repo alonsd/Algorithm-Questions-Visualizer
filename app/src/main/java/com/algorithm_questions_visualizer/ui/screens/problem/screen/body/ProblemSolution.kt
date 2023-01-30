6f92a07589d7a26156c99af666773aa38882859e
Alon Shlider <alon.shlider@gmail.com>
2023-01-30 16:46:50 +0200

    Added lines of code

--- app/src/main/java/com/algorithm_questions_visualizer/ui/screens/problem/screen/body/ProblemSolution.kt
+++ app/src/main/java/com/algorithm_questions_visualizer/ui/screens/problem/screen/body/ProblemSolution.kt
@@ -2,6 +2,7 @@ package com.algorithm_questions_visualizer.ui.screens.problem.screen.body
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,49 +19,59 @@ import com.algorithm_questions_visualizer.ui.theme.Yellow
 
 @Composable
 fun ProblemSolution(solution: String) {
-    Column(modifier = Modifier
-        .width(32.dp)
-        .fillMaxHeight()) {
-        for (i in 0..200) {
-            //TODO - add lines composable
-        }
-    }
-    AutoResizedText(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(AndroidStudioCodeBackground)
-            .padding(32.dp),
-        text = buildAnnotatedString {
-            val kotlinKeywords = mutableListOf("fun", "val", "var", "if", "return", "null", ",", "class", "while", "break", "continue")
-            val kotlinExtensions = mutableListOf("forEachIndexed")
-            solution.split(" ", ".").forEach { string ->
-                if (kotlinKeywords.contains(string)) {
-                    withStyle(
-                        style = SpanStyle(
-                            color = Orange
-                        )
-                    ) {
-                        append(string)
-                        append(' ')
-                    }
-                    return@forEach
-                } else if (kotlinExtensions.contains(string)) {
-                    withStyle(
-                        style = SpanStyle(
-                            color = Yellow
-                        )
-                    ) {
-                        append(string)
-                        append(" ")
-                    }
-                    return@forEach
+    Row(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .width(32.dp)
+                .fillMaxHeight()
+        ) {
+            LazyColumn {
+                val linesCount = solution
+                    .count { "\n".contains(it) }
+                    .coerceAtLeast(50)
+                items(linesCount) { line ->
+                    SolutionLineCode(lineOfCode = line)
                 }
-                append(string)
-                append(' ')
             }
-        },
-        color = Color.White,
-    )
+        }
+        AutoResizedText(
+            modifier = Modifier
+                .wrapContentWidth()
+                .fillMaxHeight()
+                .background(AndroidStudioCodeBackground)
+                .padding(0.dp),
+            text = buildAnnotatedString {
+                val kotlinKeywords = mutableListOf("fun", "val", "var", "if", "return", "null", ",", "class", "while", "break", "continue")
+                val kotlinExtensions = mutableListOf("forEachIndexed")
+                solution.split(" ", ".").forEach { string ->
+                    if (kotlinKeywords.contains(string)) {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Orange
+                            )
+                        ) {
+                            append(string)
+                            append(' ')
+                        }
+                        return@forEach
+                    } else if (kotlinExtensions.contains(string)) {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Yellow
+                            )
+                        ) {
+                            append(string)
+                            append(" ")
+                        }
+                        return@forEach
+                    }
+                    append(string)
+                    append(' ')
+                }
+            },
+            color = Color.White,
+        )
+    }
 }
 
 @Preview(showBackground = true)
